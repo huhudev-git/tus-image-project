ab27e5552c9c9cc6bc00358f33d283dfb998ac9c
huhu <contact@huhu.dev>
2020-12-02 11:03:09 +0900

    update

--- pixelate/mosaic_style/gauss_blur_mosaic_style.py
+++ pixelate/mosaic_style/gauss_blur_mosaic_style.py
@@ -4,7 +4,12 @@ from pixelate.mosaic_style.abstract_mosaic_style import AbstructMosaicStyle
 class GaussBlurMosaicStyle(AbstructMosaicStyle):
     """ガウスモザイクパターンのスタイルパラメータ
     """
+
+    level = None
+
+    def __init__(self, level) -> None:
+        self.level = level
     
     @classmethod
     def from_json(cls, json_data) -> "GaussBlurMosaicStyle":
-        return cls()
+        return cls(json_data['level'])
